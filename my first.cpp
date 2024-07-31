3f7e3eeac60a0eb33da233a09ffa55921eb9a7be
Percival-1 <shubhranshuranjan2004@gmail.com>
2024-07-31 23:55:54 +0530

    Sum of two numbers

---
+++ my first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    cout<<"Enter a number : ";
+    cin>>a;
+    cout<<"Enter another number ; ";
+    cin>>b;
+    cout<<"The sum of the given number is :"<<(a+b);
+    return 0;
+}
